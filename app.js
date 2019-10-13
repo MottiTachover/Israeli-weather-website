ac3c015fc0af28011a29178b1b2ea42e7ce1fb05
moti223 <mtmt334497@gmail.com>
2019-10-13 14:12:20 +0300

    add app

--- app.js
+++ app.js
@@ -9,8 +9,16 @@ const express = require("express"),
 	CityData = require("./models/cities.js"),
 	helperFunc = require("./helpers/parseDate");
 
-
-
+	mongoose.connect("mongodb+srv://devs:nav132435@cluster0-jtyln.mongodb.net/test?retryWrites=true&w=majority" || "mongodb://localhost:27017", {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useUnifiedTopology: true 
+	 }).then(()=>{
+		 console.log("Connected");
+	 }).catch(err => {
+		 console.log(err.message)
+	 });	
+	 
 setInterval(upDateDB, 1000000);
 async function upDateDB(){
 	console.log("update")
@@ -94,4 +102,4 @@ function loadTOdb(stations){
 }
 
 
-app.listen(process.env.Port || 3000,process.env.Host ||'0.0.0.0', ()=>console.log("Started"));
+app.listen(process.env.PORT || 5000,process.env.Host ||'0.0.0.0', ()=>console.log("Started"));
